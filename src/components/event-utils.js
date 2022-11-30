78a0a910c8396d83f76d7357ddecc412ad53af13
Souvik Mandal <thedevsouvik@gmail.com>
2022-11-30 10:43:17 +0000

    Update 13 files

--- src/components/event-utils.js
+++ src/components/event-utils.js
@@ -1,4 +1,4 @@
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
 
 // function events(){
 
@@ -6,31 +6,29 @@ import TodoList from './TodoList';
 //         <div  className="flex-parent-element">
 //           <div className = 'flex-child-element magenta'><TodoList /></div>
 //           {/* <div className = 'flex-child-element green'><Calender /></div> */}
-          
+
 //         </div>
-        
+
 //       );
 // }
 // export default events;
 
-let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+let eventGuid = 0;
+let todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS = [
   {
     id: createEventId(),
-    title: 'All-day event',
-    start: todayStr
+    title: "All-day event",
+    start: todayStr,
   },
   {
     id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  }
-]
+    title: "Timed event",
+    start: todayStr + "T12:00:00",
+  },
+];
 export function createEventId() {
-    console.log("creating an event ID ");
-  return  String(eventGuid++)
+  console.log("creating an event ID ");
+  return String(eventGuid++);
 }
-
-
